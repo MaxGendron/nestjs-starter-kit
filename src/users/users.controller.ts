ad70493095b7f32cd79077b59cbb660d40fe2e50
Maxime <gendron.maxime1@gmail.com>
2020-08-08 19:05:49 -0400

    Controller calling services

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Post, Body } from '@nestjs/common';
-import { QueryDto } from './models/dtos/query.dto';
+import { QueryDto } from '../models/dtos/query.dto';
 import { NewUserDto } from './models/dtos/new-user.dto';
 import { ValidateUserDto } from './models/dtos/validate-user.dto';
 import { UsersService } from './users.service';
@@ -10,17 +10,16 @@ export class UsersController {
 
   @Post()
   create(@Body() newUserDto: NewUserDto): string {
-    return "create user";
+    return this.usersService.create(newUserDto);
   }
 
   @Post('validate')
   validate(@Body() validateUserDto: ValidateUserDto): string {
-    return "validate user";
+    return this.usersService.validate(validateUserDto);
   }
 
   @Post('exist')
   exist(@Body() queryDto: QueryDto): string {
-    
-    return "user exist";
+    return this.usersService.exist(queryDto);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,8 +2,26 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './models/schemas/user.schema';
+import { NewUserDto } from './models/dtos/new-user.dto';
+import { ValidateUserDto } from './models/dtos/validate-user.dto';
+import { QueryDto } from '../models/dtos/query.dto';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  //Create a new user
+  create(newUserDto: NewUserDto) {
+    return "create user";
+  }
+
+  //Validate that the given username/password correspond to a user in the system
+  validate(validateUserDto: ValidateUserDto): string {
+    return "validate user";
+  }
+
+  //Validate if a user exist by looking at the number of document returned by the query
+  exist(queryDto: QueryDto): string {
+    return "user exist";
+  }
 }
