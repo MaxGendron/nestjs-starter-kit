6cccafc2a7d4dd0b8ce85a867e81ab41bd7570c0
Maxime <gendron.maxime1@gmail.com>
2020-08-09 20:15:42 -0400

    More detailled error on Unauthorized

--- src/users/guards/jwt-auth.guard.ts
+++ src/users/guards/jwt-auth.guard.ts
@@ -1,6 +1,20 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { CustomError } from 'src/models/custom-error';
 
-/* Use this JwtAuthGuard if you wanna restreint a route to a logged in user */
+//Use this JwtAuthGuard if you wanna restreint a route to a logged in user
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+
+  //Override handleRequest to return proper error message in case of unauthorized
+  handleRequest(err, user, info, context, status) {
+    if (err || !user) {
+      throw err || new HttpException(
+        new CustomError(HttpStatus.UNAUTHORIZED,'Unauthorized', 'Unauthorized'),
+        HttpStatus.UNAUTHORIZED
+      );
+    }
+    return user;
+  }
+}
