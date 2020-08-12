afbdc317976f190af1c64a9872b6aad103725309
Maxime <gendron.maxime1@gmail.com>
2020-08-11 22:23:35 -0400

    Validating enum

--- src/users/models/dtos/validate-user-property-value.dto.ts
+++ src/users/models/dtos/validate-user-property-value.dto.ts
@@ -1,13 +1,14 @@
-import { IsNotEmpty } from "class-validator";
-
-export class ValidateUserPropertyValueDto {
-  @IsNotEmpty()
-  property: UserPropertyEnum
-  @IsNotEmpty()
-  value: string
-}
+import { IsNotEmpty, IsEnum } from "class-validator";
 
 export enum UserPropertyEnum {
   Username = 'username',
   Email = 'email'
+}
+
+export class ValidateUserPropertyValueDto {
+  @IsNotEmpty()
+  @IsEnum(UserPropertyEnum)
+  property: UserPropertyEnum
+  @IsNotEmpty()
+  value: string
 }
