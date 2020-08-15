bf588796a01d0919cbb19335fccaff50fb443dcf
Maxime <gendron.maxime1@gmail.com>
2020-08-15 15:48:09 -0400

    Removed unused property

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -7,7 +7,6 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LocalStrategy } from './strategy/local.strategy';
-import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
   imports: [
@@ -22,7 +21,6 @@ import { JwtStrategy } from './strategy/jwt.strategy';
     }),
   ],
   controllers: [UsersController],
-  providers: [UsersService, LocalStrategy, JwtStrategy],
-  exports: [UsersService],
+  providers: [UsersService, LocalStrategy]
 })
 export class UsersModule {}
